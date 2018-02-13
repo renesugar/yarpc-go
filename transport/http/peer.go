17b1f7993d7b670276911209d4faf1fae0a63613
Kris Kowal <kris@uber.com>
2018-02-12 17:55:53 -0800

    Add jitter to suspicion window

--- transport/http/peer.go
+++ transport/http/peer.go
@@ -21,6 +21,7 @@
 package http
 
 import (
+	"math/rand"
 	"net"
 	"time"
 
@@ -89,7 +90,8 @@ func (p *httpPeer) OnSuspect() {
 	// Extend the window of innocence from the current time.
 	// Use Store instead of CAS since races at worst extend the innocence
 	// window to relatively similar distant times.
-	p.innocentUntilUnixNano.Store(now + defaultInnocenceWindow.Nanoseconds())
+	jitter := rand.Int63n(defaultInnocenceWindow.Nanoseconds())
+	p.innocentUntilUnixNano.Store(now + jitter)
 
 	// Kick the state change channel (if it hasn't been kicked already).
 	// But leave status as available.
